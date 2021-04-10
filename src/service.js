31a08cad689d8ffb26d4a8f18e69fefcd7064231
Daab <davidabrhan@hotmail.com>
2021-04-10 11:32:17 +0200

    Update service.js

--- src/service.js
+++ src/service.js
@@ -4,19 +4,22 @@ const processPlayerData = require('./utils/processPlayerData');
 
 let cachedPage = {
   lastModified: null,
-  htmlPage: null,
+  topScoreHTMLTables: null,
+  fullComboHTMLTables: null,
 };
 
 module.exports = (savePath) => {
-  let localLeaderboards; let
-    lastModified;
+  let localLeaderboards;
+  let lastModified;
 
   // load save file
   try {
     // check if cached page is actual
     lastModified = Date.parse(fs.statSync(savePath).mtime);
-    if (cachedPage.lastModified === lastModified) { return cachedPage.htmlPage; }
-
+    if (cachedPage.lastModified === lastModified) {
+      return pageHTML(cachedPage.topScoreHTMLTables, cachedPage.fullComboHTMLTables);
+    }
+    console.log('non-cached');
     // get up-to-date save file
     localLeaderboards = JSON.parse(fs.readFileSync(savePath))._leaderboardsData;
   } catch (error) {
@@ -32,9 +35,9 @@ module.exports = (savePath) => {
     const records = results.playersByTops[difficulty].map((player) => {
       const score = player.tops[difficulty] || 0;
       return recordHTML(score !== 0 && score === topScore, player.name, score);
-    });
-    return tableHTML('difficultyScore', difficulty, records.join(''));
-  });
+    }).join('');
+    return tableHTML('difficultyScore', difficulty, records);
+  }).join('');
 
   // prepare full combo HTML tables
   const fullComboHTMLTables = Object.keys(results.playersByFullCombos).map((difficulty) => {
@@ -43,12 +46,11 @@ module.exports = (savePath) => {
     const records = results.playersByFullCombos[difficulty].map((player) => {
       const score = player.fullCombos[difficulty] ? player.fullCombos[difficulty].size : 0;
       return recordHTML(score !== 0 && score === topScore, player.name, score);
-    });
-    return tableHTML('difficultyCombo', difficulty, records.join(''));
-  });
+    }).join('');
+    return tableHTML('difficultyCombo', difficulty, records);
+  }).join('');
 
   // return complete HTML page
-  const htmlPage = pageHTML(topScoreHTMLTables.join(''), fullComboHTMLTables.join(''));
-  cachedPage = { lastModified, htmlPage };
-  return htmlPage;
+  cachedPage = { lastModified, topScoreHTMLTables, fullComboHTMLTables };
+  return pageHTML(topScoreHTMLTables, fullComboHTMLTables);
 };
