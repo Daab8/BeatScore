b0cca568572a321bb6136c49ba0a7d0df74b9464
Daab <davidabrhan@hotmail.com>
2021-04-10 11:32:55 +0200

    Update and rename getLocalIP.js to utils.js

--- src/utils/getLocalIP.js
+++
@@ -1,5 +0,0 @@
-const { networkInterfaces } = require('os');
-
-module.exports = () => Object.values(networkInterfaces())
-  .reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat((i.family === 'IPv4' && !i.internal && i.address) || []), [])), [])[0]
-  || 'localhost';
