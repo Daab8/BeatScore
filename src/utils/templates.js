20eea6d1fb04d487325ee789b9fc6a374be40db1
Daab <davidabrhan@hotmail.com>
2021-04-14 09:01:50 +0200

    Update templates.js

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -4,7 +4,7 @@ exports.pageHTML = (topScores, fullCombos, changeTime, selectPlayerOptions, play
     <html lang="en">
       <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1">
+        <meta http-equiv="refresh" content="10">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>BeatScore</title>
         <style>
